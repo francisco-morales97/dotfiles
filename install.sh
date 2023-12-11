74197b45b24c049ab145b268c583e11f76fca78a
francisco-morales97 <fmorales12.97@gmail.com>
2023-12-11 18:38:11 -0300

    Actualizacion-2 11 dic

--- install.sh
+++ install.sh
@@ -8,24 +8,24 @@ NC="\033[0m"
 
 printf "${GREEN}${BOLD}Instalando mis configuraciones${NC}\n"
 sleep 1
-mv .zshenv ~/
-mv bin ~/.local/
+rsync -a .zshenv ~/
+rsync -a bin ~/.local/
 
 if [ -e ~/.config/tmux ]; then
-    mv -f xdg_config_home/tmux.conf ~/.config/tmux/
+    rsync -a xdg_config_home/tmux.conf ~/.config/tmux/
 else
     mkdir ~/.config/tmux
-    mv -f xdg_config_home/tmux.conf ~/.config/tmux/
+    rsync -a xdg_config_home/tmux.conf ~/.config/tmux/
 fi
 
-mv -f xdg_config_home/* ~/.config/
-mv -f xdg_data_home/* ~/.local/share/
+rsync -a xdg_config_home/* ~/.config/
+rsync -a xdg_data_home/* ~/.local/share/
 
 if [ -e ~/.cache ]; then
-    mv -f xdg_cache_home/* ~/.cache/
+    rsync -a xdg_cache_home/* ~/.cache/
 else
     mkdir ~/.cache
-    mv -f xdg_cache_home/* ~/.cache/
+    rsync -a xdg_cache_home/* ~/.cache/
 fi
 echo
 
